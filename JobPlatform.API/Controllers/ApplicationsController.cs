fd4d8dd1eeec06ae85365d86229104e47e3434e8
Leonid <41046801+leonid-belousov@users.noreply.github.com>
2026-05-30 23:01:16 +0500

    Expose interview invitation endpoints

--- JobPlatform.API/Controllers/ApplicationsController.cs
+++ JobPlatform.API/Controllers/ApplicationsController.cs
@@ -1,5 +1,9 @@
-﻿using JobPlatform.BLL.CQRS.Applications.Commands.ChangeApplicationStatus;
+using JobPlatform.BLL.CQRS.Applications.Commands.CancelInterviewInvitation;
+using JobPlatform.BLL.CQRS.Applications.Commands.ChangeApplicationStatus;
 using JobPlatform.BLL.CQRS.Applications.Commands.CreateApplication;
+using JobPlatform.BLL.CQRS.Applications.Commands.CreateInterviewInvitation;
+using JobPlatform.BLL.CQRS.Applications.Commands.RespondInterviewInvitation;
+using JobPlatform.BLL.CQRS.Applications.Queries.GetApplicationInterviewInvitations;
 using JobPlatform.BLL.CQRS.Applications.Queries.GetCandidateApplications;
 using JobPlatform.BLL.CQRS.Applications.Queries.GetVacancyApplications;
 using MediatR;
@@ -36,4 +40,33 @@ public class ApplicationsController : ControllerBase
     [HttpGet("by-vacancy/{vacancyId:guid}")]
     public async Task<IActionResult> ByVacancy(Guid vacancyId, CancellationToken cancellationToken)
         => Ok(await _mediator.Send(new GetVacancyApplicationsQuery(vacancyId), cancellationToken));
+
+    [HttpPost("{applicationId:guid}/interview-invitations")]
+    public async Task<IActionResult> CreateInterviewInvitation(Guid applicationId,
+        [FromBody] CreateInterviewInvitationRequest request, CancellationToken cancellationToken)
+        => Ok(await _mediator.Send(new CreateInterviewInvitationCommand(applicationId, request.ScheduledAt,
+            request.Format, request.Location, request.MeetingUrl, request.Message), cancellationToken));
+
+    [HttpGet("{applicationId:guid}/interview-invitations")]
+    public async Task<IActionResult> GetInterviewInvitations(Guid applicationId, CancellationToken cancellationToken)
+        => Ok(await _mediator.Send(new GetApplicationInterviewInvitationsQuery(applicationId), cancellationToken));
+
+    [HttpPost("interview-invitations/{invitationId:guid}/accept")]
+    public async Task<IActionResult> AcceptInterviewInvitation(Guid invitationId, CancellationToken cancellationToken)
+        => Ok(await _mediator.Send(new RespondInterviewInvitationCommand(invitationId, "accepted"), cancellationToken));
+
+    [HttpPost("interview-invitations/{invitationId:guid}/decline")]
+    public async Task<IActionResult> DeclineInterviewInvitation(Guid invitationId, CancellationToken cancellationToken)
+        => Ok(await _mediator.Send(new RespondInterviewInvitationCommand(invitationId, "declined"), cancellationToken));
+
+    [HttpPost("interview-invitations/{invitationId:guid}/cancel")]
+    public async Task<IActionResult> CancelInterviewInvitation(Guid invitationId, CancellationToken cancellationToken)
+        => Ok(await _mediator.Send(new CancelInterviewInvitationCommand(invitationId), cancellationToken));
 }
+
+public sealed record CreateInterviewInvitationRequest(
+    DateTimeOffset ScheduledAt,
+    string Format,
+    string? Location,
+    string? MeetingUrl,
+    string? Message);
