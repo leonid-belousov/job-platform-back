9e0db2548425e8992625b728ea4fb935d0265c09
Leonid <41046801+leonid-belousov@users.noreply.github.com>
2026-05-31 14:13:33 +0500

    Add auth email template contracts

--- JobPlatform.BLL/Common/Interfaces/IEmailTemplateRenderer.cs
+++ JobPlatform.BLL/Common/Interfaces/IEmailTemplateRenderer.cs
@@ -7,4 +7,8 @@ public interface IEmailTemplateRenderer
     EmailTemplateResult RenderApplicationCreated(string vacancyTitle, string candidateName, string? coverLetter);
 
     EmailTemplateResult RenderApplicationStatusChanged(string vacancyTitle, string oldStatus, string newStatus, string? comment);
+
+    EmailTemplateResult RenderEmailConfirmation(string confirmationUrl);
+
+    EmailTemplateResult RenderPasswordReset(string resetUrl);
 }
