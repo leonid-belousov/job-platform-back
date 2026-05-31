1a665568c06811b268e927214d1f96822cccceba
Leonid <41046801+leonid-belousov@users.noreply.github.com>
2026-05-31 14:20:23 +0500

    Add reset password validator

---
+++ JobPlatform.BLL/CQRS/Auth/Commands/ResetPassword/ResetPasswordCommandValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace JobPlatform.BLL.CQRS.Auth.Commands.ResetPassword;
+
+public sealed class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordCommand>
+{
+    public ResetPasswordCommandValidator()
+    {
+        RuleFor(x => x.Token).NotEmpty().MaximumLength(512);
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(128)
+            .Matches("[A-Z]").WithMessage("Password must contain an uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain a lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain a digit.");
+    }
+}
