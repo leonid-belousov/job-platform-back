f8543184d915e3a522899f6a69ac51d4856f2f47
Leonid <41046801+leonid-belousov@users.noreply.github.com>
2026-05-31 14:18:46 +0500

    Add forgot password validator

---
+++ JobPlatform.BLL/CQRS/Auth/Commands/ForgotPassword/ForgotPasswordCommandValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+
+namespace JobPlatform.BLL.CQRS.Auth.Commands.ForgotPassword;
+
+public sealed class ForgotPasswordCommandValidator : AbstractValidator<ForgotPasswordCommand>
+{
+    public ForgotPasswordCommandValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(256);
+    }
+}
