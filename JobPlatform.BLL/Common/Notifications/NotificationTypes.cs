e1e49784a677d254a6b81ce8861dea36346ac1e0
Leonid <41046801+leonid-belousov@users.noreply.github.com>
2026-05-31 14:51:29 +0500

    Expand notification types

--- JobPlatform.BLL/Common/Notifications/NotificationTypes.cs
+++ JobPlatform.BLL/Common/Notifications/NotificationTypes.cs
@@ -4,4 +4,8 @@ public static class NotificationTypes
 {
     public const string ApplicationCreated = "application.created";
     public const string ApplicationStatusChanged = "application.status_changed";
+    public const string InterviewInvitationCreated = "interview.invitation_created";
+    public const string InterviewInvitationResponded = "interview.invitation_responded";
+    public const string ReminderNewApplications = "reminder.new_applications";
+    public const string ReminderInactiveVacancy = "reminder.inactive_vacancy";
 }
